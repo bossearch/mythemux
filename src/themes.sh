cb18328110e86b361cdf0867f7cee654295d47f7
bossearch <yudha.bosse@gmail.com>
2025-05-26 20:09:08 +0800

    fix: remove unused color on themes

--- src/themes.sh
+++ src/themes.sh
@@ -1,34 +1,21 @@
 #!/usr/bin/env bash
 
 declare -A THEME=(
-  ["background"]="#1A1B26"
-  ["foreground"]="#a9b1d6"
-  ["black"]="#414868"
-  ["blue"]="#7aa2f7"
-  ["cyan"]="#7dcfff"
-  ["green"]="#73daca"
-  ["magenta"]="#bb9af7"
-  ["red"]="#f7768e"
-  ["white"]="#c0caf5"
-  ["yellow"]="#e0af68"
-
-  ["bblack"]="#2A2F41"
-  ["bblue"]="#7aa2f7"
-  ["bcyan"]="#7dcfff"
-  ["bgreen"]="#41a6b5"
-  ["bmagenta"]="#bb9af7"
-  ["bred"]="#ff9e64"
-  ["bwhite"]="#787c99"
-  ["byellow"]="#e0af68"
-
-  ['ghgreen']="#3fb950"
-  ['ghmagenta']="#A371F7"
-  ['ghred']="#d73a4a"
-  ['ghyellow']="#d29922"
-  ['ghsky']="#3B4261"
-  ['ghturquoise']="#27a1b9"
-  ['ghblack']="#16161E"
+  ["background"]="#1A1B26" #
+  ["foreground"]="#a9b1d6" # #network #wbgit
+  ["black"]="#414868" #
+  ["blue"]="#7aa2f7" # #date #music
+  ["green"]="#73daca" #
+  ["white"]="#c0caf5" #
+  ["bblack"]="#2A2F41" #
+  ["bblue"]="#7aa2f7" # #network
+  ["bgreen"]="#41a6b5" # #network
+  ['ghgreen']="#3fb950" #wbgit
+  ['ghred']="#d73a4a" #wbgit
+  ['ghyellow']="#d29922" #wbgit
+  ['ghsky']="#3B4261" # #date
+  ['ghturquoise']="#27a1b9" #
+  ['ghblack']="#16161E" # #network #music #wbgit
 )
-
 
 RESET="#[fg=${THEME[foreground]},bg=${THEME[background]},nobold,noitalics,nounderscore,nodim]"
