f408ce4e8233c6c5a75baccfc4e9e478e450e332
bossearch <yudha.bosse@gmail.com>
2026-04-02 01:30:59 +0800

    feat: migrate datetime-status.sh to base16 colors

--- src/datetime-status.sh
+++ src/datetime-status.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# shellcheck source=/dev/null
 source "$HOME/.config/tmux/theme.sh"
 
 # Exit if in a graphical session (TTY fallback check)
@@ -11,4 +12,4 @@ date_string=" %d-%m-%Y"
 time_string="%H:%M "
 separator="❬ "
 
-echo "$RESET#[fg=${THEME[blue]},bg=${THEME[ghsky]}]$date_string $separator$time_string"
+echo "$RESET#[fg=${THEME[base0D]},bg=${THEME[base02]}]$date_string $separator$time_string"
