9791b038cb69e94d17c71e5a892eec9795b7ae59
bossearch <yudha.bosse@gmail.com>
2026-06-13 12:09:05 +0800

    feat: add platform icons & new duration parsing for music-status

--- src/music-status.sh
+++ src/music-status.sh
@@ -13,25 +13,22 @@ BG_COLOR="${THEME[base00]}"
 BG_BAR="${THEME[base00]}"
 TIME_COLOR="${THEME[base0D]}"
 
-if [[ $1 =~ ^[[:digit:]]+$ ]]; then
-  MAX_TITLE_WIDTH=$1
-else
-  MAX_TITLE_WIDTH=$(($(tmux display -p '#{window_width}' 2>/dev/null || echo 120) - 90))
-fi
+MAX_TITLE_WIDTH=$(($(tmux display -p '#{window_width}' 2>/dev/null || echo 120) / 4))
 
 # playerctl
-PLAYER_STATUS=$(playerctl -a metadata --format "{{status}};{{mpris:length}};{{position}};{{title}}" | grep -m1 "Playing")
+PLAYER_STATUS=$(playerctl -a metadata --format "{{status}};{{mpris:length}};{{position}};{{artist}} - {{title}};{{xesam:url}}" | grep -m1 "Playing")
 STATUS="playing"
 
 # There is no playing media, check for paused media
 if [ -z "$PLAYER_STATUS" ]; then
-  PLAYER_STATUS=$(playerctl -a metadata --format "{{status}};{{mpris:length}};{{position}};{{title}}" | grep -m1 "Paused")
+  PLAYER_STATUS=$(playerctl -a metadata --format "{{status}};{{mpris:length}};{{position}};{{artist}} - {{title}};{{xesam:url}}" | grep -m1 "Paused")
   STATUS="paused"
 fi
 
-TITLE=$(echo "$PLAYER_STATUS" | cut -d';' --fields=4)
 DURATION=$(echo "$PLAYER_STATUS" | cut -d';' --fields=2)
 POSITION=$(echo "$PLAYER_STATUS" | cut -d';' --fields=3)
+TITLE=$(echo "$PLAYER_STATUS" | cut -d';' --fields=4)
+URL=$(echo "$PLAYER_STATUS" | cut -d';' --fields=5 | cut -d'.' -f2)
 
 # Convert position and duration to seconds from microseconds
 DURATION=$((DURATION / 1000000))
@@ -43,36 +40,49 @@ if [ "$DURATION" -eq 0 ]; then
 fi
 
 # Calculate the progress bar for sane durations
-if [ -n "$DURATION" ] && [ -n "$POSITION" ] && [ "$DURATION" -gt 0 ] && [ "$DURATION" -lt 3600 ]; then
-  TIME="[$(date -d@$POSITION -u +%M:%S) / $(date -d@$DURATION -u +%M:%S)]"
+if [ -n "$DURATION" ] && [ -n "$POSITION" ] && [ "$DURATION" -gt 0 ] && [ "$DURATION" != "$POSITION" ]; then
+  if [ "$DURATION" -lt 3600 ]; then
+    TIME="[$(date -d@$POSITION -u +%M:%S) / $(date -d@$DURATION -u +%M:%S)]"
+    BUFFER=23
+  else
+    TIME="[$(date -d@$POSITION -u +%H:%M:%S) / $(date -d@$DURATION -u +%H:%M:%S)]"
+    BUFFER=29
+  fi
+elif [ "$DURATION" = "$POSITION" ]; then
+  TIME="[󰐾 LIVE]"
+  BUFFER=16
 else
   TIME="[--:--]"
+  BUFFER=15
 fi
+
 if [ -n "$TITLE" ]; then
   if [ "$STATUS" = "playing" ]; then
-    PLAY_STATE="░ $OUTPUT"
+    if [ "$URL" = "youtube" ]; then
+      PLAY_STATE="░  $OUTPUT"
+    elif [ "$URL" = "twitch" ]; then
+      PLAY_STATE="░  $OUTPUT"
+    elif [ "$URL" = "spotify" ]; then
+      PLAY_STATE="░  $OUTPUT"
+    elif [ "$URL" = "idlixku" ] || [ "$URL" = "netflix" ]; then
+      PLAY_STATE="░ 󰝆 $OUTPUT"
+    else
+      PLAY_STATE="░  $OUTPUT"
+    fi
   else
-    PLAY_STATE="░ 󰏤$OUTPUT"
+    PLAY_STATE="░ 󰏤 $OUTPUT"
   fi
   OUTPUT="$PLAY_STATE $TITLE"
 
-  # Only show the song title if we are over $MAX_TITLE_WIDTH characters
   if [ "${#OUTPUT}" -ge $MAX_TITLE_WIDTH ]; then
-    OUTPUT="$PLAY_STATE ${TITLE:0:$MAX_TITLE_WIDTH-1}…"
+    OUTPUT="$PLAY_STATE ${TITLE:0:$MAX_TITLE_WIDTH-$BUFFER}…"
   fi
 else
   OUTPUT=''
 fi
 
-MAX_TITLE_WIDTH=25
-if [ "${#OUTPUT}" -ge $MAX_TITLE_WIDTH ]; then
-  OUTPUT="$PLAY_STATE ${TITLE:0:$MAX_TITLE_WIDTH-1}"
-  # Remove trailing spaces
-  OUTPUT="${OUTPUT%"${OUTPUT##*[![:space:]]}"}…"
-fi
-
 if [ -z "$OUTPUT" ]; then
-  echo "$OUTPUT #[fg=${THEME[base0B]},bg=default]"
+  echo ""
 else
   OUT="$OUTPUT $TIME "
   ONLY_OUT="$OUTPUT "
@@ -82,7 +92,7 @@ else
   PROGRESS=$((OUTPUT_LENGTH * PERCENT / 100))
   O="$OUTPUT"
 
-  if [ $PROGRESS -le $TIME_INDEX ]; then
+  if [ $PROGRESS -le "$TIME_INDEX" ]; then
     echo "#[nobold,fg=$BG_COLOR,bg=$ACCENT_COLOR]${O:0:PROGRESS}#[fg=$ACCENT_COLOR,bg=$BG_BAR]${O:PROGRESS:TIME_INDEX} #[fg=$TIME_COLOR,bg=$BG_BAR]$TIME "
   else
     DIFF=$((PROGRESS - TIME_INDEX))
